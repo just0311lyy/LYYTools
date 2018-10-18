b2631665e717b2533855965c84f3afda41bc7a3e
蛋定啲小渔 <392194491@qq.com>
2018-10-18 20:09:19 +0800

    1、提供基础控制器供继承用，包含键盘监听和导航栏字体、颜色设置 2、图片分类：性能更优化的图片处理方式，切图和缩放 3、新增懒加载示例

---
+++ LYYTools/Ctrls(懒加载)/LYYLazyLoadTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  LYYLazyLoadTableViewController.h
+//  LYYTools
+//
+//  Created by 李阳洋 on 2018/10/18.
+//  Copyright © 2018年 lyy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LYYLazyLoadTableViewController : UITableViewController
+
+@end
---
+++ LYYTools/Ctrls(懒加载)/LYYLazyLoadTableViewController.m
@@ -0,0 +1,101 @@
+//
+//  LYYLazyLoadTableViewController.m
+//  LYYTools
+//
+//  Created by 李阳洋 on 2018/10/18.
+//  Copyright © 2018年 lyy. All rights reserved.
+//
+
+#import "LYYLazyLoadTableViewController.h"
+
+@interface LYYLazyLoadTableViewController ()
+@property(nonatomic,strong) NSArray *groups;
+@end
+
+@implementation LYYLazyLoadTableViewController
+
+-(NSArray *)groups{
+    if (!_groups) {
+        _groups = @[@"cell01",@"cell02",@"cell03",@"cell04",@"cell05",@"cell06",@"cell07"];
+    }
+    return _groups;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    NSLog(@"%ld",_groups.count);
+    //使用懒加载，此处必须使用self,不然无法调用setter方法
+    return self.groups.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    NSString static *identifier = @"cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:identifier];
+    }
+    cell.textLabel.text = [_groups objectAtIndex:indexPath.row];
+    return cell;
+}
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
