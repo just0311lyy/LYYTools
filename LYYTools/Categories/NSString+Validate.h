83aa123d0c1e3166e099f9aa4de49edae1403178
Yangyangli <392194491@qq.com>
2020-11-19 17:28:33 +0800

    字符串内容校验：手机号，邮箱，姓名，身份证……

---
+++ LYYTools/Categories/NSString+Validate.h
@@ -0,0 +1,17 @@
+//
+//  NSString+Validate.h
+//  LYYTools
+//
+//  Created by YangyangLi on 2020/11/19.
+//  Copyright © 2020 lyy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSString (Validate)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ LYYTools/Categories/NSString+Validate.m
@@ -0,0 +1,13 @@
+//
+//  NSString+Validate.m
+//  LYYTools
+//
+//  Created by YangyangLi on 2020/11/19.
+//  Copyright © 2020 lyy. All rights reserved.
+//
+
+#import "NSString+Validate.h"
+
+@implementation NSString (Validate)
+
+@end
